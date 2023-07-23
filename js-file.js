fd82716e20d0666a1c77fb881ec748fdff059b72
Chris <129601042+Operations-Department@users.noreply.github.com>
2023-07-23 16:53:36 +0900

    Fix error display message, no longer unlimited operators in display - added to keyboard support

--- js-file.js
+++ js-file.js
@@ -14,10 +14,13 @@ const operate = (firstNumber, operator, secondNumber) => {
   if(operator === '+') {return add(firstNumber, secondNumber);}
   if(operator === '-') {return subtract(firstNumber, secondNumber);}
   if(operator === 'x') {return multiply(firstNumber, secondNumber);}
-  if(operator === '÷' && secondNumber === 0) {return "(ง'̀-'́)ง";}
+  if(operator === '÷' && secondNumber === 0) {return "error";}
   if(operator === '÷') {return divide(firstNumber, secondNumber);}
 };
 
+let decimalUsed = false; //our flag, used later to ensure only one decimal per operand
+
+//connects js to html elements
 const allButtons = document.querySelectorAll('button');
 const clearButtons = document.querySelectorAll('.clear');
 const operatorButtons = document.querySelectorAll('.operator');
@@ -27,19 +30,13 @@ const equalButton = document.getElementById('equal');
 const displayTop = document.getElementById('display-top');
 const displayBottom = document.getElementById('display-bottom');
 
-//styles when mouseover/clicked etc.
+//mouseover/click/mouseout styles
 allButtons.forEach(button => {
   button.addEventListener('mouseover', () => {
     button.classList.add('mouseover-blue');
   });
   button.addEventListener('mouseout', () => {
     button.classList.remove('mouseover-blue');
-  });
-  button.addEventListener('click', () => {
-    button.classList.add('click');
-    setTimeout(() => {
-      button.classList.remove('click')
-    }, 70);
   });
 });
 clearButtons.forEach(button => {
@@ -49,6 +46,33 @@ clearButtons.forEach(button => {
   button.addEventListener('mouseout', () => {
     button.classList.remove('mouseover-red');
   });
+  });
+operatorButtons.forEach(button => {
+  button.addEventListener('mouseover', () => {
+    button.classList.add('mouseover-operator');
+  });
+  button.addEventListener('mouseout', () => {
+    button.classList.remove('mouseover-operator');
+  });
+});  
+equalButton.addEventListener('mouseover', () => {
+  equalButton.classList.add('mouseover-equal');
+});
+equalButton.addEventListener('mouseout', () => {
+  equalButton.classList.remove('mouseover-equal');
+});
+allButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    button.classList.add('click');
+    setTimeout(() => {
+      button.classList.remove('click')
+    }, 70);
+  });
+});
+
+
+//clears display
+clearButtons.forEach(button => {
   button.addEventListener('click', () => {
     if (button.id === "ac") { 
       displayTop.textContent = "";
@@ -59,6 +83,9 @@ clearButtons.forEach(button => {
     }
   });
 });
+
+
+//add number to display when clicked
 numberButtons.forEach(button => {
   button.addEventListener('click', () => {
     const text = button.textContent;
@@ -69,29 +96,41 @@ numberButtons.forEach(button => {
     displayTop.textContent += text;
   });
 });
+
+
+decimalButton.addEventListener('click', () => {
+  if (!decimalUsed) {
+    displayTop.textContent += '.';
+    decimalUsed = true; // Set the flag to true after adding the decimal
+  }
+});
+ 
+
+//adds operator to display
 operatorButtons.forEach(button => {
-  button.addEventListener('mouseover', () => {
-    button.classList.add('mouseover-operator');
-  });
-  button.addEventListener('mouseout', () => {
-    button.classList.remove('mouseover-operator');
-  });
   button.addEventListener('click', () => {
     const text = button.textContent;
-    if (displayBottom.textContent.length > 0 && typeof parseFloat(displayBottom.textContent) === 'number') { 
+    const lastChar = displayTop.textContent.slice(-1);
+    
+    decimalUsed = false;
+
+    if (displayBottom.textContent.length > 0 && displayBottom.textContent !== "error") {
       displayTop.textContent = displayBottom.textContent;
       displayBottom.textContent = "";
+      displayTop.textContent += text;
+    } else if (lastChar === "+" || lastChar === "-" || lastChar === "x" || lastChar === "÷") {
+      displayTop.textContent = displayTop.textContent.slice(0, -1) + text; 
+    } else if (displayTop.textContent === "" && displayBottom.textContent === "" || displayBottom.textContent === "error") { //to stop putting an operator before a number
+      displayTop.textContent = "";
+      displayBottom.textContent =""; 
+    } else {
+      displayTop.textContent += text;
     }
-    displayTop.textContent += text;
   });
 });
-equalButton.addEventListener('mouseover', () => {
-  equalButton.classList.add('mouseover-equal');
-});
-equalButton.addEventListener('mouseout', () => {
-  equalButton.classList.remove('mouseover-equal');
-});
-//calculates the equation when clicked/called
+
+
+//calculates the equation - click 'equals'
 equalButton.addEventListener('click', calculateResult);
 
 // this is our big boy - calculates total
@@ -101,49 +140,82 @@ function calculateResult() {
   let operator = equation.match(symbol);
   let [firstNumber, secondNumber] = equation.split(operator[0]).map(parseFloat);
   let result = operate(firstNumber, operator[0], secondNumber);
-  //eliminates unnecessary zeroes after the decimal
-  result = result.toFixed(3).replace(/\.?0+$/, ''); 
+  
+  // Handle division by zero or other invalid operations
+  if (isNaN(result) || !isFinite(result)) {
+    displayTop.textContent = "";
+    displayBottom.textContent = "";
+    result = "error"; 
+  } else {
+    //eliminates unnecessary zeroes after the decimal - max 3 decimal places
+    result = result.toFixed(3).replace(/\.?0+$/, ''); 
+  }
+  if (displayBottom.textContent > 0) {
+    displayBottom.textContent = "";
+  }
   displayBottom.textContent += result;
 };
 
-//keyboard support
+
+//keyboard support - same syntax and operations as above
 document.addEventListener('keydown', (e) => {
   const key = e.key;
+  const lastChar = displayTop.textContent.slice(-1);
+
   //type number keys into calc
   if (/^\d$/.test(key)) {
     if(displayBottom.textContent.length > 0) {
-    displayBottom.textContent = "";
-    displayTop.textContent = "";
-  }
+      displayBottom.textContent = "";
+      displayTop.textContent = "";
+    }
     displayTop.textContent += key;
   }
+
   //type operators into calc
   if (/^[+\-]$/.test(key)) {
-    if (displayBottom.textContent.length > 0 && typeof parseFloat(displayBottom.textContent) === 'number') { 
+    if (displayBottom.textContent.length > 0) { 
       displayTop.textContent = displayBottom.textContent;
       displayBottom.textContent = "";
+    } 
+    if (lastChar === "+" || lastChar === "-" || lastChar === "x" || lastChar === "÷") {
+      displayTop.textContent = displayTop.textContent.slice(0, -1) + key;
+    } else if (displayTop.textContent === "" && displayBottom.textContent === "") {
+      displayTop.textContent = ""; //to stop putting an operator before a number
+    } else {
+      displayTop.textContent += key;
     }
-    displayTop.textContent += key;
   }
   if (key === '*') {
-    if (displayBottom.textContent.length > 0 && typeof parseFloat(displayBottom.textContent) === 'number') { 
+    if (displayBottom.textContent.length > 0) { 
       displayTop.textContent = displayBottom.textContent;
       displayBottom.textContent = "";
     }
-    displayTop.textContent += 'x';
+    if (lastChar === "+" || lastChar === "-" || lastChar === "x" || lastChar === "÷") {
+      displayTop.textContent = displayTop.textContent.slice(0, -1) + 'x';
+    } else if (displayTop.textContent === "" && displayBottom.textContent === "") {
+      displayTop.textContent = ""; //to stop putting an operator before a number
+    } else {
+      displayTop.textContent += 'x';
+    }
   }
   if (key === '/') {
-    if (displayBottom.textContent.length > 0 && typeof parseFloat(displayBottom.textContent) === 'number') { 
+    if (displayBottom.textContent.length > 0) { 
       displayTop.textContent = displayBottom.textContent;
       displayBottom.textContent = "";
+    } 
+    if (lastChar === "+" || lastChar === "-" || lastChar === "x" || lastChar === "÷") {
+      displayTop.textContent = displayTop.textContent.slice(0, -1) + '÷';
+    } else if (displayTop.textContent === "" && displayBottom.textContent === "") {
+      displayTop.textContent = ""; //to stop putting an operator before a number
+    } else {
+      displayTop.textContent += '÷';
     }
-    displayTop.textContent += '÷';
   }
   if (key === '.') {displayTop.textContent += key;}
-  //calculate total
+  //calculate total with enter key
   if (key === 'Enter') {calculateResult();} 
   //backspace
   if (key === 'Backspace') {displayTop.textContent = displayTop.textContent.slice(0, -1);} 
-  //clear all
+  //clear all with delete key
   if (key === 'Delete') {displayTop.textContent = ""; displayBottom.textContent = "";} 
 });
